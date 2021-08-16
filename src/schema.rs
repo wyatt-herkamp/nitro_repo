93e9dae46354b2398359fae41505737ed151716c
Wyatt Jacob Herkamp <wherkamp@kingtux.me>
2021-08-16 10:56:18 -0400

    Removed some code I stole from myself

--- src/schema.rs
+++ src/schema.rs
@@ -8,16 +8,6 @@ table! {
     }
 }
 table! {
-    servers (id) {
-        id -> Bigint,
-        server_id -> Text,
-        server_token ->Text,
-        settings ->Text,
-        created ->Bigint,
-
-    }
-}
-table! {
     users (id) {
         id -> Bigint,
         username -> Text,
@@ -36,61 +26,5 @@ table! {
         server -> Bigint,
         created ->Bigint,
 
-    }
-}
-table! {
-    items (id) {
-        id -> Bigint,
-        s_id -> Text,
-        name -> Text,
-        description -> Text,
-        product -> Text,
-        settings -> Text,
-        category ->Bigint,
-        server ->Bigint,
-        created ->Bigint,
-
-    }
-}
-table! {
-    auth_tokens (id) {
-        id -> Bigint,
-        user -> Bigint,
-        token -> Text,
-        expiration ->Bigint,
-        created ->Bigint,
-    }
-}
-table! {
-    sales (id) {
-        id -> Bigint,
-        s_id -> Text,
-        username -> Text,
-        uuid -> Text,
-        first_name -> Text,
-        last_name -> Text,
-        country -> Text,
-        payment -> Text,
-        cart -> Text,
-        created -> Bigint,
-    }
-}
-
-table! {
-    shopping_carts (id) {
-        id -> Bigint,
-        s_id -> Text,
-        username -> Text,
-        cart -> Text,
-        created -> Bigint,
-    }
-}
-table! {
-    forgot_passwords (id) {
-        id -> Bigint,
-        user -> Bigint,
-        token -> Text,
-        expiration ->Bigint,
-        created ->Bigint,
     }
 }
