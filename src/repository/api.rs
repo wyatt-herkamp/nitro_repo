e12f109a8b8ce506d170372474e8f5ed89f66999
Wyatt J Herkamp <wherkamp@kingtux.me>
2021-12-22 12:45:04 -0500

    Made the Repository Storage value now a text referencing the Storage name

--- src/repository/api.rs
+++ src/repository/api.rs
@@ -1,16 +1,16 @@
-use actix_web::{get, web, HttpRequest};
+use actix_web::{get, HttpRequest, web};
 use serde::{Deserialize, Serialize};
 
 use crate::api_response::SiteResponse;
+use crate::DbPool;
 use crate::error::response::not_found;
 use crate::repository::action::get_repo_by_name_and_storage;
-use crate::repository::controller::handle_result;
+use crate::repository::controller::{handle_result, to_request};
 use crate::repository::maven::MavenHandler;
 use crate::repository::models::Repository;
 use crate::repository::npm::NPMHandler;
 use crate::repository::repository::{RepositoryRequest, RepositoryType};
 use crate::storage::action::get_storage_by_name;
-use crate::DbPool;
 
 //
 
@@ -28,33 +28,17 @@ pub async fn get_versions(
     let (storage, repository, file) = path.into_inner();
     let connection = pool.get()?;
 
-    let storage = get_storage_by_name(&storage, &connection)?;
-    if storage.is_none() {
-        return not_found();
-    }
-    let storage = storage.unwrap();
-    let repository = get_repo_by_name_and_storage(&repository, &storage.id, &connection)?;
-    if repository.is_none() {
-        return not_found();
-    }
-    let repository = repository.unwrap();
 
-    let t = repository.repo_type.clone();
-    let string = file.clone();
+    let request = to_request(storage, repository, file, &connection)?;
 
-    let request = RepositoryRequest {
-        storage,
-        repository,
-        value: string,
-    };
-    let x = match t.as_str() {
+    let x = match request.repository.repo_type.as_str() {
         "maven" => MavenHandler::handle_versions(&request, &r, &connection),
         "npm" => NPMHandler::handle_versions(&request, &r, &connection),
         _ => {
             panic!("Unknown REPO")
         }
     }?;
-    handle_result(x, file, r)
+    handle_result(x, request.value, r)
 }
 
 #[get("/api/project/{storage}/{repository}/{file:.*}")]
@@ -66,32 +50,15 @@ pub async fn get_project(
     let (storage, repository, file) = path.into_inner();
     let connection = pool.get()?;
 
-    let storage = get_storage_by_name(&storage, &connection)?;
-    if storage.is_none() {
-        return not_found();
-    }
-    let storage = storage.unwrap();
-    let repository = get_repo_by_name_and_storage(&repository, &storage.id, &connection)?;
-    if repository.is_none() {
-        return not_found();
-    }
-    let repository = repository.unwrap();
+    let request = to_request(storage, repository, file, &connection)?;
 
-    let t = repository.repo_type.clone();
-    let string = file.clone();
-
-    let request = RepositoryRequest {
-        storage,
-        repository,
-        value: string,
-    };
-    let x = match t.as_str() {
+    let x = match request.repository.repo_type.as_str() {
         "maven" => MavenHandler::handle_project(&request, &r, &connection),
         _ => {
             panic!("Unknown REPO")
         }
     }?;
-    handle_result(x, file, r)
+    handle_result(x, request.value, r)
 }
 
 #[get("/api/version/{storage}/{repository}/{file:.*}")]
@@ -103,30 +70,13 @@ pub async fn get_version(
     let (storage, repository, file) = path.into_inner();
     let connection = pool.get()?;
 
-    let storage = get_storage_by_name(&storage, &connection)?;
-    if storage.is_none() {
-        return not_found();
-    }
-    let storage = storage.unwrap();
-    let repository = get_repo_by_name_and_storage(&repository, &storage.id, &connection)?;
-    if repository.is_none() {
-        return not_found();
-    }
-    let repository = repository.unwrap();
+    let request = to_request(storage, repository, file, &connection)?;
 
-    let t = repository.repo_type.clone();
-    let string = file.clone();
-
-    let request = RepositoryRequest {
-        storage,
-        repository,
-        value: string,
-    };
-    let x = match t.as_str() {
+    let x = match request.repository.repo_type.as_str() {
         "maven" => MavenHandler::handle_version(&request, &r, &connection),
         _ => {
             panic!("Unknown REPO")
         }
     }?;
-    handle_result(x, file, r)
+    handle_result(x, request.value, r)
 }
