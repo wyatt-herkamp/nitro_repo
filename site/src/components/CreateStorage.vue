1a71faed6c252b2954f96a588dfb3f26a796ebf8
Wyatt J Herkamp <wherkamp@kingtux.me>
2022-01-05 14:00:51 -0500

    Storage list and Storage Create outlined

--- site/src/components/CreateStorage.vue
+++ site/src/components/CreateStorage.vue
@@ -1,24 +1,123 @@
-<template></template>
+<template>
+  <div>
+    <vue-final-modal
+      v-model="showModel"
+      classes="flex justify-center items-center"
+    >
+      <div
+        class="
+          relative
+          border
+          bg-white
+          dark:bg-slate-900
+          border-slate-100
+          dark:border-black
+          m-w-20
+          py-5
+          px-10
+          rounded-2xl
+          shadow-xl
+          text-center
+        "
+      >
+        <p class="font-bold text-xl pb-4">Create Storage</p>
+        <form class="flex flex-col w-96 <sm:w-65" @submit.prevent="onSubmit()">
+          <div class="mb-4">
+            <label
+              class="block text-slate-50 text-sm font-bold mb-2"
+              for="name"
+            >
+              Storage ID/Name
+            </label>
+            <input
+              id="name"
+              v-model="form.name"
+              autocomplete="off"
+              class="
+                shadow
+                appearance-none
+                border
+                rounded
+                w-full
+                py-2
+                px-3
+                text-gray-700
+                leading-tight
+                focus:outline-none focus:shadow-outline
+              "
+              placeholder="Storage ID/Name"
+              type="text"
+            />
+          </div>
+          <div class="mb-4">
+            <label
+              class="block text-slate-50 text-sm font-bold mb-2"
+              for="name"
+            >
+              Storage Public Name
+            </label>
+            <input
+              id="name"
+              v-model="form.public_name"
+              autocomplete="off"
+              class="
+                shadow
+                appearance-none
+                border
+                rounded
+                w-full
+                py-2
+                px-3
+                text-gray-700
+                leading-tight
+                focus:outline-none focus:shadow-outline
+              "
+              placeholder="Public Name"
+              type="text"
+            />
+          </div>
+          <button
+            class="
+              bg-slate-100
+              dark:bg-slate-800
+              py-2
+              my-3
+              rounded-md
+              cursor-pointer
+              text-white
+            "
+          >
+            Create Storage
+          </button>
+        </form>
+
+        <button class="absolute top-0 right-0 mt-5 mr-5" @click="close()">
+          🗙
+        </button>
+      </div>
+    </vue-final-modal>
+    <div @click="showModel = true">
+      <slot name="button"></slot>
+    </div>
+  </div>
+</template>
 
 <script lang="ts">
-import {Storage} from "@/backend/Response";
-import {defineComponent, ref} from "vue";
-import {createNewStorage} from "@/backend/api/admin/Storage";
+import { Storage } from "@/backend/Response";
+import { defineComponent, ref } from "vue";
+import { createNewStorage } from "@/backend/api/admin/Storage";
 
 export default defineComponent({
-  props: {
-    updateList: {
-      required: true,
-      type: Function,
-    },
-  },
   setup() {
+    const showModel = ref(false);
+    const close = () => (showModel.value = false);
+
     let form = ref({
       name: "",
       public_name: "",
       error: "",
     });
-    return { form };
+    return { form, showModel, close };
   },
   methods: {
     async onSubmit() {
@@ -29,7 +128,6 @@ export default defineComponent({
       );
       if (response.ok) {
         let data = response.val as Storage;
-        this.$props.updateList(data.id);
         this.$notify({
           title: "Storage Created",
           type: "success",
--- site/src/views/admin/Storages.vue
+++ site/src/views/admin/Storages.vue
@@ -1,26 +1,20 @@
 <template>
-  <SideBar />
+  <div class="flex flex-row">
+    <SideBar />
+    <Storages />
+  </div>
 </template>
 
 <script lang="ts">
-import {defineComponent, onBeforeMount, ref} from "vue";
+import { defineComponent, onBeforeMount, ref } from "vue";
 import Storages from "@/components/Storages.vue";
-import Users from "@/components/Users.vue";
-import Me from "@/components/Me.vue";
-import Repositories from "@/components/Repositories.vue";
 import SideBar from "@/components/admin/SideBar.vue";
-import Settings from "@/components/Settings.vue";
-import UpdateUser from "@/components/UpdateUser.vue";
+
 import userStore from "@/store/user";
 
 export default defineComponent({
   components: {
     Storages,
-    Repositories,
-    Users,
-    UpdateUser,
-    Settings,
-    Me,
     SideBar,
   },
 
@@ -34,10 +28,3 @@ export default defineComponent({
   },
 });
 </script>
-
-<style>
-.el-menu-vertical-demo:not(.el-menu--collapse) {
-  width: 200px;
-  min-height: 400px;
-}
-</style>
