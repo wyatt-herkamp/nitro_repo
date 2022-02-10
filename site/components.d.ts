75e32f1822076553e59f627dfff83ddfe254f7e8
Wyatt Jacob Herkamp <wherkamp@kingtux.me>
2022-02-09 21:21:42 -0500

    Depend Updates

--- site/components.d.ts
+++ site/components.d.ts
@@ -3,25 +3,25 @@
 // Read more: https://github.com/vuejs/vue-next/pull/3399
 
 declare module 'vue' {
-    export interface GlobalComponents {
-        CreateRepo: typeof import('./src/components/CreateRepo.vue')['default']
-        CreateStorage: typeof import('./src/components/CreateStorage.vue')['default']
-        CreateUser: typeof import('./src/components/CreateUser.vue')['default']
-        Item: typeof import('./src/components/admin/Item.vue')['default']
-        Login: typeof import('./src/components/nav/Login.vue')['default']
-        MavenUpload: typeof import('./src/components/upload/MavenUpload.vue')['default']
-        Me: typeof import('./src/components/Me.vue')['default']
-        MenuButton: typeof import('./src/components/nav/MenuButton.vue')['default']
-        Navbar: typeof import('./src/components/nav/Navbar.vue')['default']
-        Repositories: typeof import('./src/components/Repositories.vue')['default']
-        Settings: typeof import('./src/components/Settings.vue')['default']
-        SideBar: typeof import('./src/components/admin/SideBar.vue')['default']
-        Storages: typeof import('./src/components/Storages.vue')['default']
-        UpdateStorage: typeof import('./src/components/UpdateStorage.vue')['default']
-        UpdateUser: typeof import('./src/components/UpdateUser.vue')['default']
-        Users: typeof import('./src/components/Users.vue')['default']
-        ViewRepo: typeof import('./src/components/repo/ViewRepo.vue')['default']
-    }
+  export interface GlobalComponents {
+    CreateRepo: typeof import('./src/components/CreateRepo.vue')['default']
+    CreateStorage: typeof import('./src/components/CreateStorage.vue')['default']
+    CreateUser: typeof import('./src/components/CreateUser.vue')['default']
+    Item: typeof import('./src/components/admin/Item.vue')['default']
+    Login: typeof import('./src/components/nav/Login.vue')['default']
+    MavenUpload: typeof import('./src/components/upload/MavenUpload.vue')['default']
+    Me: typeof import('./src/components/Me.vue')['default']
+    MenuButton: typeof import('./src/components/nav/MenuButton.vue')['default']
+    Navbar: typeof import('./src/components/nav/Navbar.vue')['default']
+    Repositories: typeof import('./src/components/Repositories.vue')['default']
+    Settings: typeof import('./src/components/Settings.vue')['default']
+    SideBar: typeof import('./src/components/admin/SideBar.vue')['default']
+    Storages: typeof import('./src/components/Storages.vue')['default']
+    UpdateStorage: typeof import('./src/components/UpdateStorage.vue')['default']
+    UpdateUser: typeof import('./src/components/UpdateUser.vue')['default']
+    Users: typeof import('./src/components/Users.vue')['default']
+    ViewRepo: typeof import('./src/components/repo/ViewRepo.vue')['default']
+  }
 }
 
-export {}
+export { }
