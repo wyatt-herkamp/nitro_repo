ca51a8a77636492393a2f591265dff0b24a56667
Wyatt Herkamp <wherkamp@gmail.com>
2022-07-03 20:03:57 -0400

    Bug Fixes

--- backend/src/system/web/public.rs
+++ backend/src/system/web/public.rs
@@ -1,6 +1,7 @@
 use actix_web::cookie::Cookie;
 use actix_web::web;
 use actix_web::{post, HttpRequest, HttpResponse};
+use actix_web::http::StatusCode;
 use log::error;
 use sea_orm::DatabaseConnection;
 use serde::{Deserialize, Serialize};
@@ -23,15 +24,20 @@ pub async fn login(
     nc: web::Json<Login>,
 ) -> actix_web::Result<HttpResponse> {
     let user = verify_login(nc.username.clone(), nc.password.clone(), &connection).await??;
-    let cookie: Cookie = r.cookie("session").unwrap();
-    actix_web::rt::spawn(async move {
-        if (session_manager.set_user(cookie.value(), user.id).await).is_err() {
-            error!(
+    let cookie = r.cookie("session");
+    if let Some(cookie) = cookie {
+        actix_web::rt::spawn(async move {
+            if (session_manager.set_user(cookie.value(), user.id).await).is_err() {
+                error!(
                 "Unable to save user {} to cookie {}",
                 user.id,
                 cookie.value()
             );
-        }
-    });
-    Ok(HttpResponse::Ok().json(user))
+            }
+        });
+        Ok(HttpResponse::Ok().json(user))
+    } else {
+        log::warn!("No cookie found for {r:?}");
+        Ok(HttpResponse::Ok().status(StatusCode::BAD_GATEWAY).finish())
+    }
 }
