e1df429bab729debead1d5b1b575e7e249804e18
Wyatt Herkamp <wherkamp@gmail.com>
2022-08-02 22:32:56 -0400

    Updating the Error Stuff some more

--- backend/src/error/internal_error.rs
+++ backend/src/error/internal_error.rs
@@ -3,154 +3,43 @@ use std::str::ParseBoolError;
 use std::string::FromUtf8Error;
 use std::time::SystemTimeError;
 
+use crate::storage::error::StorageError;
+use crate::system::permissions::PermissionError;
 use actix_web::http::StatusCode;
 use actix_web::HttpResponse;
 use base64::DecodeError;
+use this_actix_error::ActixError;
 use thiserror::Error;
-use crate::system::permissions::PermissionError;
 
 /// Errors that happen internally to the system.
 /// Not as a direct result of a Request
 #[derive(Error, Debug, ActixError)]
 pub enum InternalError {
     #[error("JSON error {0}")]
-    JSONError(serde_json::Error),
+    JSONError(#[from] serde_json::Error),
     #[error("IO error {0}")]
-    IOError(std::io::Error),
+    IOError(#[from] std::io::Error),
     #[error("DB error {0}")]
-    DBError(sea_orm::error::DbErr),
+    DBError(#[from] sea_orm::error::DbErr),
     #[error("Boolean Parse Error")]
-    BooleanParseError(ParseBoolError),
+    BooleanParseError(#[from] ParseBoolError),
     #[error("Decode Error")]
-    DecodeError(DecodeError),
+    DecodeError(#[from] DecodeError),
     #[error("UTF Decode Error")]
-    UTF8Error(FromUtf8Error),
-    #[error("SMTP Error")]
-    SMTPTransportError(#[from]lettre::transport::smtp::Error),
-    #[error("Missing Argument {0}")]
-    MissingArgument(String),
-    #[error("Not Found")]
-    NotFound,
-    SMTPTransportError(lettre::transport::smtp::Error),
+    UTF8Error(#[from] FromUtf8Error),
     #[error("Internal Error {0}")]
     Error(String),
     #[error("Missing Config Value {0}")]
     ConfigError(String),
     #[error("Storage Error: {0}")]
-    StorageError(StorageError),
+    StorageError(#[from] StorageError),
     #[error("Invalid Repository Type {0}")]
     InvalidRepositoryType(String),
     #[error("Permission Error: {0}")]
-    PermissionError(crate::system::permissions::PermissionError),
-    #[error("THE INTERNAL WEBSITE HAS BROKEN DOWN. PLEASE REPORT to https://github.com/wherkamp/nitro_repo and restart application")]
-    DeadSite,
+    PermissionError(#[from] crate::system::permissions::PermissionError),
 }
-impl ResponseError for InternalError {}
-impl From<StorageError> for InternalError {
-    fn from(storage_error: StorageError) -> Self {
-        InternalError::StorageError(storage_error)
-    }
-}
-
-impl actix_web::error::ResponseError for InternalError {
-    fn error_response(&self) -> HttpResponse {
-        self.json_error()
-    }
-}
-
-//from<Error>
-impl From<PermissionError> for InternalError {
-    fn from(err: PermissionError) -> InternalError {
-        InternalError::PermissionError(err)
-    }
-}
-
-impl From<DecodeError> for InternalError {
-    fn from(err: DecodeError) -> InternalError {
-        InternalError::DecodeError(err)
-    }
-}
-
-impl From<chrono::ParseError> for InternalError {
-    fn from(err: chrono::ParseError) -> InternalError {
-        InternalError::Error(err.to_string())
-    }
-}
-
-impl From<FromUtf8Error> for InternalError {
-    fn from(err: FromUtf8Error) -> InternalError {
-        InternalError::UTF8Error(err)
-    }
-}
-
-impl From<Box<dyn Error>> for InternalError {
-    fn from(err: Box<dyn Error>) -> InternalError {
-        InternalError::Error(err.to_string())
-    }
-}
-
-impl From<diesel::result::Error> for InternalError {
-    fn from(err: diesel::result::Error) -> InternalError {
-        InternalError::DBError(err)
-    }
-}
-
-impl From<r2d2::Error> for InternalError {
-    fn from(err: r2d2::Error) -> InternalError {
-        InternalError::R2D2Error(err)
-    }
-}
-
 impl From<argon2::password_hash::Error> for InternalError {
     fn from(err: argon2::password_hash::Error) -> InternalError {
         InternalError::Error(err.to_string())
-    }
-}
-
-impl From<handlebars::RenderError> for InternalError {
-    fn from(err: handlebars::RenderError) -> InternalError {
-        InternalError::Error(err.to_string())
-    }
-}
-
-impl From<serde_json::Error> for InternalError {
-    fn from(err: serde_json::Error) -> InternalError {
-        InternalError::JSONError(err)
-    }
-}
-
-impl From<actix_web::Error> for InternalError {
-    fn from(err: actix_web::Error) -> InternalError {
-        InternalError::ActixWebError(err)
-    }
-}
-
-impl From<SystemTimeError> for InternalError {
-    fn from(err: SystemTimeError) -> InternalError {
-        InternalError::Error(err.to_string())
-    }
-}
-
-impl From<std::io::Error> for InternalError {
-    fn from(err: std::io::Error) -> InternalError {
-        InternalError::IOError(err)
-    }
-}
-
-impl From<lettre::transport::smtp::Error> for InternalError {
-    fn from(err: lettre::transport::smtp::Error) -> InternalError {
-        InternalError::SMTPTransportError(err)
-    }
-}
-
-impl From<ParseBoolError> for InternalError {
-    fn from(err: ParseBoolError) -> InternalError {
-        InternalError::BooleanParseError(err)
-    }
-}
-
-impl From<&str> for InternalError {
-    fn from(error: &str) -> Self {
-        InternalError::Error(error.to_string())
     }
 }
