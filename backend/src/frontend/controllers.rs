3a2b2e1fe289d029d543c0143e6481b8ef5b1508
Wyatt Herkamp <wherkamp@kingtux.me>
2022-08-18 10:18:37 -0400

    Windows Installer Work

--- backend/src/frontend/controllers.rs
+++ backend/src/frontend/controllers.rs
@@ -1,12 +1,12 @@
-use std::fs::read_to_string;
-use std::path::Path;
+use std::fs::{create_dir_all, File, read_to_string};
+use std::path::{Path, PathBuf};
 
 use actix_files::Files;
 use actix_web::error::ErrorInternalServerError;
 use actix_web::web::Data;
 use actix_web::{web, HttpResponse};
 use handlebars::Handlebars;
-use log::{debug, trace, warn};
+use log::{debug, error, info, trace, warn};
 use serde_json::json;
 
 use crate::NitroRepoData;
@@ -14,7 +14,23 @@ use crate::NitroRepoData;
 pub fn init(cfg: &mut web::ServiceConfig) {
     debug!("Loading Frontend!");
     let frontend_string = std::env::var("FRONTEND").unwrap_or_else(|_| "frontend".to_string());
-    let frontend_path = Path::new(&frontend_string);
+    let frontend_path = if frontend_string.ends_with(".zip") {
+        let zip_to = Path::new(frontend_string.trim_end_matches(".zip"));
+        info!("Unzipping frontend to {}", zip_to.display());
+        if !zip_to.exists() {
+            create_dir_all(zip_to).unwrap();
+            let zip_path = Path::new(&frontend_string);
+            if let Err(error) = extract(zip_to, zip_path) {
+                error!("Error extracting frontend: {}",error);
+            }
+            zip_to.to_path_buf()
+        } else {
+            info!("Frontend already unzipped");
+            zip_to.to_path_buf()
+        }
+    } else {
+        Path::new(&frontend_string).to_path_buf()
+    };
     let index = frontend_path.join("index.html");
     trace!("Frontend Path {}", frontend_path.display());
     if !frontend_path.exists() {
@@ -50,4 +66,48 @@ pub async fn frontend_handler(
         .render("index", &value)
         .map_err(ErrorInternalServerError)?;
     return Ok(HttpResponse::Ok().content_type("text/html").body(content));
+}
+
+fn extract(
+    extract_to: impl AsRef<Path>,
+    archive: impl AsRef<Path>,
+) -> Result<(), std::io::Error>
+{
+    let file = File::open(&archive)?;
+
+    let mut archive = zip::ZipArchive::new(file)?;
+
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i)?;
+        let outpath = match file.enclosed_name() {
+            Some(path) => {
+                let components = path.components();
+                let buf = components.skip(1).fold(PathBuf::default(), |buf, component| {
+                    buf.join(component)
+                });
+                extract_to.as_ref().join(buf)
+            }
+            None => continue,
+        };
+
+        if (*file.name()).ends_with('/') {
+            debug!("File {} extracted to \"{}\"", i, outpath.display());
+            create_dir_all(&outpath)?;
+        } else {
+            debug!(
+                "File {} extracted to \"{}\" ({} bytes)",
+                i,
+                outpath.display(),
+                file.size()
+            );
+            if let Some(p) = outpath.parent() {
+                if !p.exists() {
+                    create_dir_all(&p)?;
+                }
+            }
+            let mut outfile = File::create(&outpath)?;
+            std::io::copy(&mut file, &mut outfile)?;
+        }
+    }
+    Ok(())
 }
